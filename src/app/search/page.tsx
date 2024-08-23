91bb79c9ab6d0d3b9594aa7e93d7fa8c6bbb6d97
KolyaMilk <Kolyamilk@mail.ru>
2024-08-23 15:29:54 +0300

    edit css in page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -9,7 +9,6 @@ type Joke = {
   url: string;
   created_at: string;
 }
-
 type JokesData = {
   total: number;
   result: Joke[];
@@ -18,9 +17,9 @@ const Search = () => {
   const [query, setQuery] = useState<string>('');
   const [data, setData] = useState<JokesData | null>(null);
 
+
   useEffect(() => {
     let isMounted = true;
-
     const fetchData = async () => {
       if (query && query.length >= 4) {
         const response = await fetch(`https://api.chucknorris.io/jokes/search?query=${query}`);
@@ -44,10 +43,10 @@ const Search = () => {
   return (
     <main className={styles.mainBlock}>
       <div className={styles.form__group}>
-        <input type="text" className={styles.form__input} id="name" placeholder="Find the joke"  value={query} onChange={handleInputChange} />
+        <input type="text" className={styles.form__input} id="name" placeholder="Find the joke" value={query} onChange={handleInputChange} />
         <span>{data ? 'total: ' + data.total : ''}</span>
       </div>
-      <section className={styles.resultBlock}>
+      <section>
         {query && data && data.result ? (
           <ul className={styles.resultBlock}>
             {data.result.map((item: Joke) => (
@@ -68,5 +67,4 @@ const Search = () => {
     </main >
   );
 }
-
 export default Search;
